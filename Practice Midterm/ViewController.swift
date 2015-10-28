e8c81757f2f0ca7bc2484e5a02433adac09315ed
Hinck, Johann A <f00312298@s114d-02-osx-3.local>
2015-10-27 20:12:05 -0500

    Initial Commit

---
+++ Practice Midterm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice Midterm
+//
+//  Created by Hinck, Johann A on 10/27/15.
+//  Copyright © 2015 Hinck, Johann A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
